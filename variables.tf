2b84bd6152054ff0e598bdef09fc16993e0ef699
Narian318 <73864396+Narian318@users.noreply.github.com>
2024-01-20 21:32:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "naren-tf-s3-787"
+  default     = "naren-tf-s3-786"
 }
 
 variable "acl" {
