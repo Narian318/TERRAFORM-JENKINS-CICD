1f229c417b80ec7e4040b875db7dc133ab3a44be
Narian318 <73864396+Narian318@users.noreply.github.com>
2024-01-20 22:21:37 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -64,6 +64,6 @@ resource "aws_s3_bucket_website_configuration" "website" {
   error_document {
     key = "error.html"
   }
-
-  depends_on = [ aws_s3_bucket_acl.example.id ]
 }
+  depends_on = [ aws_s3_bucket_acl.example.id ]
+
