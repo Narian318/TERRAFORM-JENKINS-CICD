ba4bbfa9262aaa2cc1e11421396364dff2522437
Narian318 <73864396+Narian318@users.noreply.github.com>
2024-01-20 22:09:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "mrcloudbook-777-ajaykumar-yegireddi"
+  default     = "naren-s3-2024-Jan"
 }
 
 
