906bb6d5acc409193552d7660d10f52c722f0a77
Narian318 <73864396+Narian318@users.noreply.github.com>
2024-01-20 22:06:34 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,33 +2,6 @@ resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucketname
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.mybucket.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.mybucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
-  ]
-
-  bucket = aws_s3_bucket.mybucket.id
-  acl    = "public-read"
-}
-
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.mybucket.id
   key = "index.html"
@@ -53,13 +26,6 @@ resource "aws_s3_object" "style" {
   content_type = "text/css"
 }
 
-resource "aws_s3_object" "script" {
-  bucket = aws_s3_bucket.mybucket.id
-  key = "script.js"
-  source = "script.js"
-  acl = "public-read"
-  content_type = "text/javascript"
-}
 
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.mybucket.id
@@ -71,5 +37,5 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 
-  depends_on = [ aws_s3_bucket_acl.example.id ]
+  
 }
