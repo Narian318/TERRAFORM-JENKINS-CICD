219b45b8c934d5aaabbbda4e48415a7b13499a45
Narian318 <73864396+Narian318@users.noreply.github.com>
2024-01-20 19:53:54 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "nn-tf-s3-staticwebsite255"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "nn-tflock-dynamodb-2024"
   }
 }
